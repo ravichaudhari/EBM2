34e22dba2f0791728826b54a2f7fb3b22b135edc
“ravichaudhari” <ravichaudhari33@gmail.com>
2017-04-09 23:42:19 +0530

    Initial commit

---
+++ app/src/main/java/com/project/ebm/ebm/MainActivity.java
@@ -0,0 +1,17 @@
+package com.project.ebm.ebm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void myFunction(){
+        int i=2;
+    }
+}
